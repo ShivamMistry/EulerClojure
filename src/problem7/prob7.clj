9306b0a6d645aeeb5c655f9860b6320eabd9f983
Shivam Mistry <admin@shivam-mistry.co.uk>
2014-02-01 20:33:48 +0000

    optimising problem 7 more

--- src/problem7/prob7.clj
+++ src/problem7/prob7.clj
@@ -1,6 +1,6 @@
 (defn prime? [n]
     (if (and (not (= n 2)) (even? n)) false (if (= n 2) true
-        (not (some #(zero? (mod n %)) (range 3 (inc (Math/ceil (Math/sqrt n))) 2)))
+        (not-any? #(zero? (mod n %)) (range 3 (inc (Math/ceil (Math/sqrt n))) 2))
 )))
 
-(println (nth (filter #(prime? %) (iterate (partial + 2) 1)) (dec 10001)))
+(println (nth (filter prime? (iterate (partial + 2) 1)) (dec 10001)))
